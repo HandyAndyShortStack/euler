f5aed2d30c213f532150790bf92a22f2d95b3cac
Andy Short <andynshort@gmail.com>
2013-05-02 18:04:05 -0700

    another javascript solution that didnt pan out

--- 16/solution.js
+++ 16/solution.js
@@ -6,12 +6,29 @@ function getDigitLength(number) {
 	return digits;
 }
 
-digit_sum = 0;
-working_number = Math.pow(2, 1000);
-while (working_number) {
-	left_digit = parseInt(working_number.toString()[0], 10);
-	working_number -= left_digit * Math.pow(10, getDigitLength(working_number) - 1);
-	digit_sum += left_digit;
+function chopFirstDigit(number) {
+	var left_digit = parseInt(number.toString()[0]);
+	return number - (left_digit * Math.pow(10, getDigitLength(number) - 1));
 }
 
-console.log(digit_sum);
+function getDigit(index) {
+	var working_number = 2;
+	for (var i = 0; i < 999; i += 1) {
+		working_number = working_number * 2;
+		if (working_number >= Math.pow(10, index)) {
+			working_number = chopFirstDigit(working_number);
+		}
+	}
+	if (working_number < Math.pow(10, index - 1)) {
+		return 0;
+	}
+	return parseInt(working_number.toString()[0]);
+}
+
+var sum = '';
+digit_length = getDigitLength(Math.pow(2, 1000));
+for (var i = 1; i <= digit_length; i += 1) {
+	sum += getDigit(i);
+}
+
+console.log(sum.split("").reverse().join(""));
