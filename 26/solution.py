840e79124fcab2117733b7758257b9fdd5cbe526
Andy Short <andynshort@gmail.com>
2014-03-10 16:01:12 -0700

    now only off by one

--- 26/solution.py
+++ 26/solution.py
@@ -2,11 +2,17 @@ def find_length_of_repeating_segment_of_unit_fraction(number):
   remainders = {}
   index = 0
   remainder = 0.1
-  while remainder not in remainders:
-    tup = divmod(remainder * 10, number)
-    remainder = tup[1]
+  while True:
+    remainder = (int(remainder * 10) % number)
+    if remainder in remainders:
+      return index - remainders[remainder]
     remainders[remainder] = index
     index += 1
-    print remainder, index
-  return index - remainders[remainder]
 
+greatest_repeating_segment_length = 0
+for i in range(1, 1000):
+  segment_length = find_length_of_repeating_segment_of_unit_fraction(i)
+  if segment_length > greatest_repeating_segment_length:
+    greatest_repeating_segment_length = segment_length
+
+print greatest_repeating_segment_length
