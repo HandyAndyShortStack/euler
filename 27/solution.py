375ecd7db4670ba782b6afb76db6ab27299fbbc2
Andy Short <andynshort@gmail.com>
2014-03-27 15:04:28 -0700

    working python solution

---
+++ 27/solution.py
@@ -0,0 +1,31 @@
+import math
+
+def isPrime(number):
+  if number < 2:
+    return False
+  for i in range(2, int(math.ceil(math.sqrt(number)))):
+    if number % i == 0:
+      return False
+  return True
+
+def result(a, b, n):
+  return (n ** 2) + (a * n) + b
+  
+def findNumberOfPrimes(a, b):
+  count = 0
+  i = 0
+  while isPrime(result(a, b, i)):
+    count += 1
+    i += 1
+  return count
+
+mostPrimes = 0
+product = 0
+for a in range(-999, 1000):
+  for b in range(-999, 1000):
+    numberOfPrimes = findNumberOfPrimes(a, b)
+    if numberOfPrimes > mostPrimes:
+      mostPrimes = numberOfPrimes
+      product = a * b
+
+print product
