612d0797b56377bd1b4224564fc0d6ab5a17ede3
handyandyshortstack <andynshort@gmail.com>
2014-05-22 15:44:35 -0700

    not accounting for incomplete sets in result strumberray. also slow

--- 32/solution.rb
+++ 32/solution.rb
@@ -1,4 +1,5 @@
 strumbers = %w(1 2 3 4 5 6 7 8 9)
+solutions = []
 
 (1..7).each do |multiplicand_digits|
   max_multiplier_digits =  7 - multiplicand_digits
@@ -7,8 +8,16 @@ strumbers = %w(1 2 3 4 5 6 7 8 9)
     (1..max_multiplier_digits).each do |multiplier_digits|
       strumbers.permutation(multiplier_digits).each do |multiplier_strumberray|
         multiplier = multiplier_strumberray.reduce(:+).to_i
-        product = multiplicand * mulitplier
+        product = multiplicand * multiplier
         product_strumberray = product.to_s.split("")
         strumberray = product_strumberray + multiplicand_strumberray + multiplier_strumberray
         result = strumberray.join("")
-        strumbers.each { |strumber| if result }
+        strumbers.each { |strumber| result.sub!(strumber, "") }
+        solutions << product if result.empty?
+      end
+    end
+  end
+  puts multiplicand_digits
+end
+
+puts solutions.uniq.reduce :+
